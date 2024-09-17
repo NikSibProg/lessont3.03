3afe93938cb29bd68ad997db00825dda876f46c6
NikSibProg <nikolajbusiness@gmail.com>
2024-09-17 19:35:14 +0300

    add functions

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ running = True
 while running:
     screen.fill(color)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # Исправлено
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Исправлено условие проверки попадания по цели
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
